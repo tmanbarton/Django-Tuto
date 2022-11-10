e4c1f62c035d9e1157d2b851345751f93c56a7ba
tmanbarton <tmanbarton@gmail.com>
2022-11-10 16:26:43 -0700

    initial commit

---
+++ mysite/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+from .models import Book
+
+def index(request):
+    return HttpResponse("Hello, world!")
+
+
+def book_by_id(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    return render(request, 'myapp/book_details.html', {'book': book})
