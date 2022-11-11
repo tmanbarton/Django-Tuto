0492dcc51d6bf36bf8469589cd5bf31b9f8b422c
tmanbarton <tmanbarton@gmail.com>
2022-11-10 18:59:49 -0700

    changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'RdBxDwUldrdWybAIGpRu',
+        'HOST': 'containers-us-west-112.railway.app',
+        'PORT': '6031',
     }
 }
 
